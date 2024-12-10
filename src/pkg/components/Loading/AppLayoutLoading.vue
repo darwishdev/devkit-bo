f480a002a32cce572d3e9b7ddca1b25a395423d1
KareemWebDeveloper <116171727+KareemWebDeveloper@users.noreply.github.com>
2024-12-10 23:48:44 +0200

    layout loading

--- src/pkg/components/Loading/AppLayoutLoading.vue
+++ src/pkg/components/Loading/AppLayoutLoading.vue
@@ -2,42 +2,52 @@
 // import TableLoading from '@/components/loading/TableLoading.vue';
 import Skeleton from 'primevue/skeleton';
 </script>
+
 <template>
-    <div class="hidden md:flex w-full absolute left-0 top-0">
-        <div class="relative w-8rem">
-            <Skeleton width="6rem" height="100vh" class="mb-2"></Skeleton>
-            <div class="my-2 absolute m-auto top-0 mx-3" style="top : 5%;">
-                <Skeleton shape="circle" size="4rem"></Skeleton>
-            </div>
-            <div class="absolute m-auto" style="top: 20%;">
-                <Skeleton width="3rem" height="2.8rem" class="mx-4"></Skeleton>
-            </div>
-            <div class="absolute m-auto" style="top: 30%;">
-                <Skeleton width="3rem" height="2.8rem" class="mx-4"></Skeleton>
-            </div>
-            <div class="absolute m-auto" style="top: 40%;">
-                <Skeleton width="3rem" height="2.8rem" class="mx-4"></Skeleton>
-            </div>
-            <div class="absolute m-auto" style="top: 50%;">
-                <Skeleton width="3rem" height="2.8rem" class="mx-4"></Skeleton>
-            </div>
-        </div>
+  <div class="hidden md:flex w-full absolute left-0 top-0">
+    <div class="relative w-[8rem]">
+      <Skeleton width="6rem" height="100vh" class="mb-2"></Skeleton>
+      <div class="absolute top-[5%] mx-3 my-2">
+        <Skeleton shape="circle" size="4rem"></Skeleton>
+      </div>
+      <div class="absolute top-[20%] mx-4">
+        <Skeleton width="3rem" height="2.8rem"></Skeleton>
+      </div>
+      <div class="absolute top-[30%] mx-4">
+        <Skeleton width="3rem" height="2.8rem"></Skeleton>
+      </div>
+      <div class="absolute top-[40%] mx-4">
+        <Skeleton width="3rem" height="2.8rem"></Skeleton>
+      </div>
+      <div class="absolute top-[50%] mx-4">
+        <Skeleton width="3rem" height="2.8rem"></Skeleton>
+      </div>
     </div>
-    <div class="w-full md:w-11 m-auto absolute top-0 right-0">
-        <div class="flex justify-content-between md:justify-content-end align-items-center m-5 border-2 p-4 border-round"
-            style="border-color: rgba(255, 255, 255, 0.05);">
-            <div class="flex md:hidden align-items-center">
-                <Skeleton width="1.5rem" height="2rem" class="mx-1"></Skeleton>
-                <Skeleton shape="circle" size="3rem" class="mx-1"></Skeleton>
-            </div>
-            <div class="flex">
-                <Skeleton width="2rem" height="2rem" class="mx-1"></Skeleton>
-                <Skeleton width="2rem" height="2rem" class="mx-1"></Skeleton>
-                <Skeleton width="2rem" height="2rem" class="mx-1"></Skeleton>
-            </div>
-        </div>
+  </div>
+
+  <div class="w-full md:w-[91.666667%] mx-auto absolute top-0 right-0">
+    <div
+      class="flex justify-between md:justify-end items-center m-5 border-2 p-4 rounded-lg"
+      style="border-color: rgba(255, 255, 255, 0.05);"
+    >
+      <div class="flex md:hidden items-center">
+        <Skeleton width="1.5rem" height="2rem" class="mx-1"></Skeleton>
+        <Skeleton shape="circle" size="3rem" class="mx-1"></Skeleton>
+      </div>
+      <div class="flex">
+        <Skeleton width="2rem" height="2rem" class="mx-1"></Skeleton>
+        <Skeleton width="2rem" height="2rem" class="mx-1"></Skeleton>
+        <Skeleton width="2rem" height="2rem" class="mx-1"></Skeleton>
+      </div>
     </div>
-    <div class="main mt-5 m-auto" style="width : 80%; padding-top: 10vh;">
-        <!-- <TableLoading /> -->
-    </div>
+  </div>
+
+  <div class="main mt-5 mx-auto" style="width: 80%; padding-top: 10vh;">
+    <!-- <TableLoading /> -->
+  </div>
 </template>
+<style>
+.p-skeleton:where([class*="my-app-dark"], [class*="my-app-dark"] *){
+  background-color: var(--p-glass) !important;
+}
+</style>
